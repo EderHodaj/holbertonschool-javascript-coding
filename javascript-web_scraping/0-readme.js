05ef150e995f443fa7c1c80c3e78d70b870f3856
EderHodaj <eder0302@gmail.com>
2024-05-13 13:11:42 +0200

    all

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(content);
 });
