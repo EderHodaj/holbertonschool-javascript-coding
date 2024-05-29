7d43d7f2af178fcc8f551421b37aaa575d147899
EderHodaj <eder0302@gmail.com>
2024-05-29 11:36:17 +0200

    node

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,42 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
+const urlApi = 'https://swapi.co/api/films/' + process.argv[2];
 
-const movieId = process.argv[2];
+request(urlApi, function (error, response, body) {
+  if (error) {
+    console.error(error); // Print the error if one occurred
+  }
+  const charUrl = JSON.parse(body).characters;
 
-if (!movieId) {
-    console.log("Please provide a movie ID.");
-    process.exit(1);
-}
-
-const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request.get(movieUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
-    }
-    if (response.statusCode !== 200) {
-        console.error('Request failed with status:', response.statusCode);
-        return;
-    }
-
-    const movieData = JSON.parse(body);
-    const charactersUrls = movieData.characters;
-
-    charactersUrls.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error('Error:', error);
-                return;
-            }
-            if (response.statusCode !== 200) {
-                console.error('Request failed with status:', response.statusCode);
-                return;
-            }
-
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
-        });
+  for (let i = 0; i < charUrl.length; i++) {
+    request(charUrl[i], function (error, response, body) {
+      if (error) {
+        console.error(error); // Print the error if one occurred
+      }
+      console.log(JSON.parse(body).name);
     });
+  }
 });
